45b2dfb8bef90c0c81024b602459a5ca2bb8e321
AnuragBhattacharjee04 <bhattacharjeeanurag000@gmail.com>
2025-11-10 04:05:24 +0530

    signin post request

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 const{Router}= require("express");
-const {User} = require("../models/user");
+const User = require("../models/user");
 const router = Router();
 
 router.get("/signin",(req,res)=>{
@@ -9,6 +9,15 @@ router.get("/signin",(req,res)=>{
 router.get("/signup",function(req,res){
    return res.render('Signup');
 
+});
+
+router.post('/signin', async function(req,res){
+    const{email,password} = req.body;
+    const isMatched = await User.matchedPassword(email,password);
+    console.log("successfuly matched user:" ,isMatched)
+
+    
+    return res.redirect('/');
 });
 
 router.post('/signup',async(req,res)=>{
